303d0dfd4be255c977c197316b10ebdedbca6235
Wxq <Wxq@123.cn>
2026-02-24 14:01:55 +0800

    基础axios的封装

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { getCategoryAPI } from '@/api/http'
 
 import App from './App.vue'
 import router from './router'
+getCategoryAPI().then(res => {
+    console.log(res);
+})
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
 
