7d99a4530be8b199a51caa1660b5fa6c719c9dce
Wxq <Wxq@123.cn>
2026-02-24 13:30:14 +0800

    elment ui 主题色定制

--- vite.config.js
+++ vite.config.js
@@ -18,8 +18,11 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    }),
+      resolvers: [
+        // 1.配置Element Plus采用saas样式配色系统
+        ElementPlusResolver({ importStyle: 'sass' }),
+      ],
+    })
   ],
   resolve: {
     alias: {
@@ -27,4 +30,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2.自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
