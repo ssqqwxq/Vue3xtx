76374194e66e68bf19b6a203d628b33996f378f1
Wxq <Wxq@123.cn>
2026-02-25 15:01:57 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,5 +15,8 @@ import homeProduct from './components/HomeProduct.vue'
         <HomeNew />
         <HomeHot />
         <homeProduct />
+        <!-- 测试 -->
+        <HomePanel title="新鲜好物" subTitle="新鲜好物 好多商品"></HomePanel>
+        <HomePanel title="热门好物" subTitle="热门好物 好多商品"></HomePanel>
     </div>
 </template>
