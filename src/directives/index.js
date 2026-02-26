506a6943a8f9b2b9a31369c38b3d4fc78a66aa1c
Wxq <Wxq@123.cn>
2026-02-26 10:57:59 +0800

    图片懒加载封装成插件引入mainJs文件

---
+++ src/directives/index.js
@@ -0,0 +1,27 @@
+
+// 把懒加载定义成插件引入到main.js中全局使用
+export const lazyPlugin = {
+    install(app) {     //   install(app){}固定写法
+        // 自定义指令 图片懒加载逻辑
+        app.directive('img-lazy', {
+            mounted(el, binding) {
+                // el：绑定指令的img元素  binding.value：真实图片地址（比如item.picture）
+                // 1. 创建视口监听器
+                // IntersectionObserver 创建监听器 lists是被监听到的4张图
+                const imgobserver = new IntersectionObserver((lists) => {
+                    // console.log(lists);
+                    const list = lists[0] //  只取一项
+                    // 2. 图片进入视口时    isIntersecting = true 就是用户进入视口
+                    if (list.isIntersecting) {
+                        // 把真实地址赋值给src，触发图片请求
+                        el.src = binding.value
+                        // 加载完后停止监听，避免重复触发
+                        imgobserver.unobserve(el)
+                    }
+                })
+                // 3. 监听当前img元素
+                imgobserver.observe(el)
+            }
+        })
+    }
+}
--- src/main.js
+++ src/main.js
@@ -5,38 +5,17 @@ import '@/styles/common.scss'// 全局样式
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { getCategoryAPI } from '@/api/http'
-
 import App from './App.vue'
 import router from './router'
 // getCategoryAPI().then(res => {
 //     console.log(res);
 // })//测试接口
+import { lazyPlugin } from '@/directives/index' //图片懒加载插件
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
+app.use(lazyPlugin)
 
-// 自定义指令 图片懒加载逻辑
-app.directive('img-lazy', {
-    mounted(el, binding) {
-        // el：绑定指令的img元素  binding.value：真实图片地址（比如item.picture）
-        // 1. 创建视口监听器
-        // IntersectionObserver 创建监听器 lists是被监听到的4张图
-        const imgobserver = new IntersectionObserver((lists) => {
-            // console.log(lists);
-            const list = lists[0] //  只取一项
-            // 2. 图片进入视口时    isIntersecting = true 就是用户进入视口
-            if (list.isIntersecting) {
-                // 把真实地址赋值给src，触发图片请求
-                el.src = binding.value
-                // 加载完后停止监听，避免重复触发
-                imgobserver.unobserve(el)
-            }
-        })
-        // 3. 监听当前img元素
-        imgobserver.observe(el)
-    }
-})
 
