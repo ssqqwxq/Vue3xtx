178e6c31689d398a24e6b0bc7925e6d0ee701750
Wxq <Wxq@123.cn>
2026-03-02 21:27:57 +0800

    token失效重新登录

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 // 创建axios实例
 const instance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -23,7 +24,15 @@ instance.interceptors.request.use(config => {
 // axios响应式拦截器
 instance.interceptors.response.use(res => res.data, e => {
     // e=>{...} 请求错误回调
+    console.log(e.response);
     ElMessage.warning(e.response.data.message)
+    // 401 token过期或失效 清除token重新登录
+    if (e.response.status === 401) { // 可选链操作，避免undefined报错
+        console.log('触发401跳转')
+        const userStore = useUserStore()
+        userStore.clearUserInfo()
+        router.push('/login')
+    }
     return Promise.reject(e)
 })
 
