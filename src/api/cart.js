23955bb890d65fcf4c16601fc56083eb70da0dc4
Wxq <Wxq@123.cn>
2026-03-05 13:21:43 +0800

    接口购物车登录时添加与删除

---
+++ src/api/cart.js
@@ -0,0 +1,25 @@
+import instance from '@/utils/http'
+// 获取购物车
+export const addCartApi = (data) => {
+    return instance({
+        url: '/member/cart',
+        method: 'post',
+        data
+    })
+}
+// 获取最新购物车列表
+export const getCartList = () => {
+    return instance({
+        url: '/member/cart'
+    })
+}
+// 删除购物车
+export const delCartApi = (ids) => {
+    return instance({
+        url: '/member/cart',
+        method: 'DELETE',
+        data: {
+            ids
+        }
+    })
+}
--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,33 +1,61 @@
 // 购物车
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
+import { useUserStore } from '@/stores/user'
+import { addCartApi, getCartList, delCartApi } from '@/api/cart'
+
 export const useCartStore = defineStore('cart', () => {
-    // 1. 购物车数组
+    const userStore = useUserStore()
+    const isLogin = computed(() => userStore.userInfo.token) //  每次访问isLogin.value，都会实时去userStore里拿当前最新的 token 值。
+    //  购物车数组
     const cartList = ref([])
-    // 2. 添加
-    const getCart = (goods) => {
-        // 添加购物车操作
-        // 已添加过 - count + 1
-        // 没有添加过 - 直接push
-        // 思路：通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
-        const item = cartList.value.find((item) => item.skuId === goods.skuId)
-        if (item) {
-            // 找到了
-            item.count = item.count + goods.count
+
+    const upCartList = async () => {
+        const res = await getCartList() // 获取最新购物车列表
+        cartList.value = res.result     // 最新购物车列表覆盖本地
+    }
+    //  添加
+    const getCart = async (goods) => {
+        // 登录
+        if (isLogin.value) {
+            // 登录之后的加入购车逻辑
+            await addCartApi(goods)   // 添加购物车
+            upCartList()
         } else {
-            // 没找到
-            cartList.value.push(goods)
+            // 没登陆 就是本地操作
+            // 添加购物车操作
+            // 已添加过 - count + 1
+            // 没有添加过 - 直接push
+            // 思路：通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
+            const item = cartList.value.find((item) => item.skuId === goods.skuId)
+            if (item) {
+                // 找到了
+                item.count = item.count + goods.count
+            } else {
+                // 没找到
+                cartList.value.push(goods)
+            }
         }
+
     }
     // 删除商品
-    const delCart = (skuId) => {
-        // 筛选 所有 ！==传来的skuId 的item赋值给 cartList.value
-        cartList.value = cartList.value.filter((item) => item.skuId !== skuId)
+    const delCart = async (skuId) => {
+        if (isLogin.value) {
+            // 登陆时
+            await delCartApi([skuId]) // 删除购物车
+            upCartList()
+        } else {
+            // 未登录时
+            // 筛选 所有 ！==传来的skuId 的item赋值给 cartList.value
+            cartList.value = cartList.value.filter((item) => item.skuId !== skuId)
+        }
     }
+
     // 全部选中
     const allCheck = (selected) => {
         cartList.value.forEach((item) => item.selected = selected)
     }
+
     //1 计算属性   计算购物车总数量
     const allCount = computed(() => cartList.value.reduce((sum, item) => sum + item.count, 0))
     //2 计算属性   购物车总价钱
@@ -41,6 +69,7 @@ export const useCartStore = defineStore('cart', () => {
     const selectCount = computed(() => cartList.value.filter((item) => item.selected).reduce((sum, item) => sum + item.count, 0))
     //5 计算属性  已选择价格
     const selectPrice = computed(() => cartList.value.filter((item) => item.selected).reduce((sum, item) => sum + item.count * item.price, 0))
+
     //  单选控制全选计算属性   每一项都是true every才会返回true  单选控制全选
     const isAll = computed(() => cartList.value.every((item) => item.selected))
     return {
